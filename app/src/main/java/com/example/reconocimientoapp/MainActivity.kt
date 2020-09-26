04156d047eb4037df67bc3c80529243cdaed9022
Martin <martinbirman@gmail.com>
2020-09-26 15:02:29 -0300

    Added bottom_nav

--- app/src/main/java/com/example/reconocimientoapp/MainActivity.kt
+++ app/src/main/java/com/example/reconocimientoapp/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.reconocimientoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->  
+        when(item.itemId){
+            R.id.home -> {
+                println("home pressed")
+                replaceFragment(HomeFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.face -> {
+                println("face pressed")
+                replaceFragment(FaceFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.cons -> {
+                println("cons pressed")
+                replaceFragment(ConsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_navbar.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        replaceFragment(HomeFragment())
     }
+
+    private fun replaceFragment (fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
