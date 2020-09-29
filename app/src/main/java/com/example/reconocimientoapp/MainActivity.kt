31c3589dd2c68f89cedc3d6933fc7ab51c3fa7ce
genaro10 <genarolorio10@gmail.com>
2020-09-28 22:38:04 -0300

    reconocimiento

--- app/src/main/java/com/example/reconocimientoapp/MainActivity.kt
+++ app/src/main/java/com/example/reconocimientoapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.reconocimientoapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.drawToBitmap
+import com.camerakit.CameraKit
+import com.camerakit.CameraKitView
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var detector: FirebaseVisionFaceDetector? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        vista.open()
+
+        val realTimeOpts = FirebaseVisionFaceDetectorOptions.Builder()
+            .setContourMode(FirebaseVisionFaceDetectorOptions.ALL_CONTOURS)
+            .build()
+
     }
 }
