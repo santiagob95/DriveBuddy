016a2bbd5d66f47eb8d5fe63ac3461f08335cf55
genaro10 <genarolorio10@gmail.com>
2020-10-07 21:54:16 -0300

    Probando camera x api android

--- app/src/main/java/com/example/reconocimientoapp/FaceFragment.kt
+++ app/src/main/java/com/example/reconocimientoapp/FaceFragment.kt
@@ -1,26 +1,30 @@
 package com.example.reconocimientoapp
 
-import android.content.ContentValues.TAG
-import android.graphics.Bitmap
+
+import android.content.pm.PackageManager
+import android.graphics.Camera
+import android.media.Image
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
+
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.CameraX
+import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.CameraView
-import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.vision.CameraSource
-import com.google.android.gms.vision.Frame
-import com.google.firebase.ml.vision.common.FirebaseVisionImage
-import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
 import kotlinx.android.synthetic.main.fragment_face.*
+import java.io.File
+import java.lang.Exception
+import kotlin.math.log
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,39 +40,105 @@ class FaceFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var preview : Preview?=null
+    private var imageCapture:ImageCapture?=null
+    private var camera:Camera?=null
 
+    private lateinit var outputDirectory:File
+    private lateinit var btnTakePhoto:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        buton.setOnClickListener { takePhoto() }
+        if(allPermissionsGranted()){
+            startCamera()
+        }else{
+            ActivityCompat.requestPermissions(
+                requireActivity(),REQUIRED_PERMISSIONS,REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode=== REQUEST_CODE_PERMISSIONS){
+            if(allPermissionsGranted()){
+                startCamera()
+            }else{
+
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            requireContext(),it)==PackageManager.PERMISSION_GRANTED
+
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+            } catch(exc: Exception) {
+                Log.e("a", "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun takePhoto(){
+
+    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_face, container, false)
     }
 
-    override fun onStart() {
-        super.onStart()
 
-    }
 
 
 
 
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
+
+        private const val FILENAME_FORMAT="yyyy-MM-dd-HH-mm-ss"
+        private const val REQUEST_CODE_PERMISSIONS=10
+        private val REQUIRED_PERMISSIONS= arrayOf(android.Manifest.permission.CAMERA)
+
+
         // TODO: Rename and change types and number of parameters
         @JvmStatic fun newInstance(param1: String, param2: String) =
             FaceFragment().apply {
