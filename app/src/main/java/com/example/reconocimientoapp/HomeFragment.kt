8609bfc9473c115b65487968d255deef072c732a
santiagob95 <santiagob95@hotmail.com>
2020-10-14 00:39:25 -0300

    Busca los datos del usuario en Firestore

--- app/src/main/java/com/example/reconocimientoapp/HomeFragment.kt
+++ app/src/main/java/com/example/reconocimientoapp/HomeFragment.kt
@@ -1,12 +1,14 @@
 package com.example.reconocimientoapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -15,7 +17,7 @@ import kotlinx.android.synthetic.main.fragment_home.*
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 private var auth: FirebaseAuth = Firebase.auth
-
+private val db = FirebaseFirestore.getInstance()
 /**
  * A simple [Fragment] subclass.
  * Use the [HomeFragment.newInstance] factory method to
@@ -36,7 +38,17 @@ class HomeFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        emailText.text=auth.currentUser!!.email.toString()
+        val userRef = db.collection("users").document(auth.currentUser!!.uid)
+
+        userRef.get()
+            .addOnSuccessListener {docSnapshot ->
+                    val userDoc = docSnapshot.data
+                    val welcomeText = "Hola, " + userDoc!!.getValue("nomYApe") +"\nmail: " + userDoc.getValue("email") +"\nid: " + userDoc.getValue("id")
+                    emailText.text = welcomeText
+
+
+            }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
