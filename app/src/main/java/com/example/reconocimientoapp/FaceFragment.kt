f15706c65965bc29b158bed78c487e522cb67879
Martin <martinbirman@gmail.com>
2020-10-28 09:54:44 -0300

    fix HomeFragment.kt

--- app/src/main/java/com/example/reconocimientoapp/FaceFragment.kt
+++ app/src/main/java/com/example/reconocimientoapp/FaceFragment.kt
@@ -7,6 +7,7 @@ import android.content.Context.VIBRATOR_SERVICE
 import android.content.pm.PackageManager
 import android.graphics.*
 import android.media.Image
+import android.media.MediaRecorder.VideoSource.SURFACE
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.*
@@ -464,7 +465,7 @@ class FaceFragment : Fragment()  {
         override fun analyze(imageProxy: ImageProxy) {
             val mediaImage = imageProxy?.image
             if (mediaImage != null) {
-                val image = FirebaseVisionImage.fromMediaImage(mediaImage,0)
+                val image = FirebaseVisionImage.fromMediaImage(mediaImage,Surface.ROTATION_270)
                 mListener.setOnLumaListener(image)
                 imageProxy.close()
             }
