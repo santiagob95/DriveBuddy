c94406ec63deb9a4290ce8fbb25b12176f6efb05
Martin <martinbirman@gmail.com>
2020-11-01 10:24:37 -0300

    implementación del modal

---
+++ app/src/main/java/com/example/reconocimientoapp/MyCustomDialog.kt
@@ -0,0 +1,23 @@
+package com.example.reconocimientoapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class MyCustomDialog: DialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        getDialog()!!.getWindow()?.setBackgroundDrawableResource(R.drawable.round_corner);
+        return inflater.inflate(R.layout.modaldialog, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val width = (resources.displayMetrics.widthPixels * 0.85).toInt()
+        val height = (resources.displayMetrics.heightPixels * 0.40).toInt()
+        dialog!!.window?.setLayout(width, height)
+    }
+
+}
