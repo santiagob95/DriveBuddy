e9bd0eb95eb7fba845501634db5c8431315ab2c2
Tomas <tomascertoma@gmail.com>
2020-11-01 00:39:49 -0300

    Implementado tts en la pagina de consejos.

--- app/src/main/java/com/example/reconocimientoapp/RecyclerAdapter.kt
+++ app/src/main/java/com/example/reconocimientoapp/RecyclerAdapter.kt
@@ -1,32 +1,62 @@
 package com.example.reconocimientoapp
 
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
-class RecyclerAdapter (private var titulos: List<String>, private var contenidos: List<String>, private var imagenes: List<Int>) :
+
+class RecyclerAdapter(
+    private var titulos: List<String>,
+    private var contenidos: List<String>,
+    private var imagenes: List<Int>
+) :
 RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), TextToSpeech.OnInitListener{
         val itemTitulo: TextView = itemView.findViewById(R.id.textView3)
         val itemContenido: TextView = itemView.findViewById(R.id.textView4)
         val itemImagen: ImageView = itemView.findViewById(R.id.imageView2)
+        private var tts: TextToSpeech? = null;
 
         init {
+            itemView.isEnabled = false;
+            tts = TextToSpeech(itemView.context, this)
+
             itemView.setOnClickListener { v: View ->
-                val position: Int = adapterPosition
-                Toast.makeText(itemView.context, "Este es el consejo # ${position + 1}", Toast.LENGTH_SHORT).show()
+                processTextToSpeech()
             }
+        }
+
+        override fun onInit(status: Int) {
+            if(status == TextToSpeech.SUCCESS){
+                val locSpanish = Locale("spa", "MEX")
+                val result = tts!!.setLanguage(locSpanish)
+                if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED ){
+                    Log.e("TTS", "¡No se puede reproducir el lenguaje especificado!")
+                }else {
+                    itemView.isEnabled = true;
+                }
+            }else{
+                Log.e("TTS", "¡Falló la inicialización!")
+            }
+        }
+
+        private fun processTextToSpeech(){
+            val position: Int = adapterPosition
+            val text = contenidos[position]
+            tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_layout,parent,false)
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
         return ViewHolder(v)
     }
 
@@ -39,5 +69,6 @@ RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
     override fun getItemCount(): Int {
         return titulos.size
     }
+
 
 }
