2aff570818fe664d6c03b759345f513a763eb804
genaro10 <genarolorio10@gmail.com>
2020-11-04 16:14:56 -0300

    Guardo

--- app/src/main/java/com/example/reconocimientoapp/ConfiguracionDialog.kt
+++ app/src/main/java/com/example/reconocimientoapp/ConfiguracionDialog.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +13,7 @@ import androidx.fragment.app.DialogFragment
 import com.squareup.okhttp.internal.Internal.instance
 import kotlinx.android.synthetic.main.modaldialog.*
 import kotlinx.android.synthetic.main.modaldialog.view.*
+import kotlinx.android.synthetic.main.pausadialog.*
 
 
 class ConfiguracionDialog: DialogFragment() {
@@ -23,11 +25,16 @@ class ConfiguracionDialog: DialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
     override fun onStart() {
         super.onStart()
         val width = (resources.displayMetrics.widthPixels * 0.95).toInt()
         val height = (resources.displayMetrics.heightPixels * 0.60).toInt()
         dialog!!.window?.setLayout(width, height)
+        switchNotificacion.setOnClickListener {
+            Log.e("a","a")
+        }
     }
+
 }
